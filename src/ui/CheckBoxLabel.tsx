601eeda0776b2938a81a9e45b7f7a551987e7bf1
CA01971172 <CA01971172@st.kawahara.ac.jp>
2025-09-02 17:35:42 +0900

    Implement CheckBoxLabel

---
+++ src/ui/CheckBoxLabel.tsx
@@ -0,0 +1,22 @@
+import { Checkbox, FormControlLabel } from "@mui/material";
+
+type Props = {
+    label: string;
+    isChecked: boolean;
+    setIsChecked: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function CheckBoxLabel( props: Props ){
+    const { label, isChecked, setIsChecked } = props;
+    return (
+        <FormControlLabel
+            label={label}
+            control={
+                <Checkbox
+                    checked={isChecked}
+                    onChange={() => setIsChecked(prev => !prev)}
+                />
+            }
+        />
+    )
+}
