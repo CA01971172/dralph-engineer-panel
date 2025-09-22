93d8a49556626313ab0609b69e0bc91989daec5b
CA01971172 <CA01971172@st.kawahara.ac.jp>
2025-09-22 10:31:59 +0900

    Divide by components

---
+++ src/content/EditModal/NamesField.tsx
@@ -0,0 +1,79 @@
+import { IconButton, Modal, Paper, TextField, Theme, ThemeProvider } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useContext } from "react";
+import { DataContext } from "../DataProvider";
+
+export default function NamesField(){
+    const {
+        armorIndex,
+        setArmorIndex,
+        data,
+        setData
+    } = useContext(DataContext);
+
+    return (
+        <>
+            <div>
+                <TextField
+                    label="搭乗者名"
+                    variant="standard"
+                    sx={{
+                        "& input::placeholder": {
+                        color: "white",       // プレースホルダーの色
+                        opacity: 1,         // opacity を1にして色をはっきりさせる
+                        },
+                    }}
+                    value={data.characterName}
+                    onChange={(e) => {
+                        setData({ ...data, characterName: e.target.value });
+                    }}
+                />
+                <IconButton
+                    color="primary"
+                    onClick={() => {
+                        setData(prev => ({
+                            ...prev,
+                            powerArmors: [
+                                ...prev.powerArmors,
+                                { armorName: "" }
+                            ]
+                        }))
+                    }}
+                >
+                    <AddIcon/>
+                </IconButton>
+            </div>
+            {(data.powerArmors || []).map((armor, index) => (
+                <div>
+                    <TextField
+                        key={index}
+                        variant="standard"
+                        label="アーマー名"
+                        value={armor.armorName}
+                        onChange={(e) => {
+                            const newArmors = [...data.powerArmors];
+                            newArmors[index].armorName = e.target.value;
+                            setData({ ...data, powerArmors: newArmors });
+                        }}
+                    />
+                    <IconButton
+                        color="primary"
+                        onClick={() => {
+                            if(armorIndex >= index) setArmorIndex(prev => {
+                                if(prev === 0) return 0;
+                                return prev - 1;
+                            });
+                            setData(prev => ({
+                                ...prev,
+                                powerArmors: prev.powerArmors.filter((_, i) => i !== index)
+                            }));
+                        }}
+                    >
+                        <DeleteIcon/>
+                    </IconButton>
+                </div>
+            ))}
+        </>
+    );
+}
